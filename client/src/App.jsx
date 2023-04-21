449c7c12e78e40fa09a30b6fcadbdd7f7582f398
vanshita shah <vanshita.s@simformsolutions.com>
2023-04-21 16:29:43 +0530

    initial Configuration and Container UI is added

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app-body">
+      <div className="container">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
