66edb02e1854b79aef0ae5e5758cd64cdb873304
vanshita shah <vanshita.s@simformsolutions.com>
2023-04-21 16:58:28 +0530

    DateHeader component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import DateHeader from './Components/DateHeader'
 import './App.css'
 
 const App = () => {
   return (
     <div className="app-body">
       <div className="container">
-
+        <DateHeader />
       </div>
     </div>
   )
